d375e2fe04d4e40d76a285d6e3fc57d785d83623
Gabriel <biel_borel@hotmail.com>
2022-08-17 19:15:32 -0300

    Some fixes on header/header css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,11 @@
 import styles from './Header.module.scss';
+import igniteLogo from '../../assets/Ignite-logo.svg';
 
 export const Header = () => {
-  return <header className={styles.header}>hehe</header>;
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt='Logo do Ignite' />
+      <strong>Ignite Feed</strong>
+    </header>
+  );
 };
