ef422b428140a6d93a149fe547f662d2813ef51d
Gabriel <biel_borel@hotmail.com>
2022-08-17 19:14:36 -0300

    Sidebar Component created

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,31 @@
+import styles from './Sidebar.module.scss';
+import { PencilLine } from 'phosphor-react';
+
+export const Sidebar = () => {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src='https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNwYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src='https://avatars.githubusercontent.com/u/69679068?v=4'
+          alt='Gabriel Borel avatar'
+        />
+
+        <strong>Gabriel Borel</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href='#'>
+          <PencilLine weight='bold' size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+};
